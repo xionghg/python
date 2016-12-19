3ea3440d7539607aac112f49d8449b4549f54277
xiong <xionghg@users.noreply.github.com>
2016-12-19 11:54:42 +0800

    update
    update

--- translate/translate.py
+++ translate/translate.py
@@ -9,17 +9,16 @@ import urllib
 import random
 
 class StringLine(object):
-	key = "UnknownString"
-	value = "UnknownString"
-	lineNumber = 0
-	isTranslated = False
-	def __init__(self, key, value, lineNumber, isTranslated):
+	def __init__(self, key, msgid, value, lineNumber, isTranslated):
 		self.key = key
+		self.msgid = msgid
 		self.value = value
 		self.lineNumber = lineNumber
 		self.isTranslated = isTranslated
 	def setKey(self, key):
 		self.key = key
+	def setMsgid(self, msgid):
+		self.msgid = msgid
 	def setValue(self, value):
 		self.value = value
 	def setLineNumber(self, lineNumber):
@@ -37,7 +36,7 @@ def trans(query):
 
 	q = query
 	fromLang = 'zh'
-	toLang = 'en'
+	toLang = 'cht'
 	salt = random.randint(32768, 65536)
 
 	sign = appid+q+str(salt)+secretKey
@@ -51,7 +50,7 @@ def trans(query):
 	    httpClient.request('GET', myurl)
 	    #response是HTTPResponse对象
 	    response = httpClient.getresponse()
-	    print(response.read())
+	    return response.read().decode("unicode_escape")
 	except Exception as e:
 	    print(e)
 	finally:
@@ -60,32 +59,6 @@ def trans(query):
 
 
 def other():
-	# str = 'asdf.asdfasdf.xasdf.xml'
-	# strs = str.split('.xml')[0]
-	# print(len(strs))
-	# print(type(strs))
-	# for s in strs:
-	# 	print(s)
-	# print(re.match("rlovep","rlovep.com"))
-
-	# line = ' <string-array name="deleteNote" product="asdf">\n删除</string>'
-	# print(line)
-	# regex = '<string.+name\s*=\s*"(?P<name>[^"]+)("\s*product\s*=\s*"(?P<product>[^"]+))?".*>(?P<value>.*)</string'
-	# pattern = re.compile(regex, re.S)
-	# # use match to find pair in line head,use search instead
-	# m = pattern.search(line)
-	# print("m:")
-	# print(m)
-	# if m:
-	# 	print(m.group(0))
-	# 	print(m.group('name'))
-	# 	print(m.group('product'))
-	# 	print(m.group('value'))
-
-	# line2 = 'fishc'
-	# pattern2 = re.compile('[a-z]+')
-	# m2 = pattern2.match(line2)
-	# print(m2)
 	content = []
 	content.append('asdf')
 	content.append('aswrewer')
@@ -124,7 +97,7 @@ def readFile(fileName):
 def readFileToDict(fileName):
 	inFile = open(fileName,"r",encoding="utf-8")
 	dict = {}
-	regex = '<string.+name\s*=\s*"(?P<name>[^"]+("\s*product\s*=\s*"(?P<product>[^"]+))?)".*>(?P<value>.*)</string'
+	regex = '\s*<string.+name\s*=\s*"(?P<name>[^"]+("\s*product\s*=\s*"(?P<product>[^"]+))?)"[^>]*>(?P<value>.*)</string'
 	pattern = re.compile(regex, re.S)
 
 	read = inFile.readline()
@@ -132,11 +105,10 @@ def readFileToDict(fileName):
 		if 'string' in read:
 			while '</string' not in read:
 				read += inFile.readline()
-			m = pattern.search(read)
+			m = pattern.match(read)
 			if m:
-				dict[m.group('name')] = m.group('value')
+				dict[m.group('name')] = read
 		read = inFile.readline()
-
 	return dict
 
 
@@ -144,7 +116,7 @@ def readFileToArray(fileName):
 	inFile = open(fileName,"r",encoding="utf-8")
 	array = []
 	lineNumber = 0
-	regex = '<string.+name\s*=\s*"(?P<name>[^"]+("\s*product\s*=\s*"(?P<product>[^"]+))?)".*>(?P<value>.*)</string'
+	regex = '\s*<string.+name\s*=\s*"(?P<name>[^"]+("\s*product\s*=\s*"(?P<product>[^"]+))?)("\s*msgid\s*=\s*"(?P<msgid>[^"]+))?"[^>]*>(?P<value>.*)</string'
 	pattern = re.compile(regex, re.S)
 
 	read = inFile.readline()
@@ -155,43 +127,112 @@ def readFileToArray(fileName):
 			while '</string' not in read:
 				read += inFile.readline()
 				temp += 1
-			m = pattern.search(read)
+			m = pattern.match(read)
 			if m:
-				array.append(StringLine(m.group('name'),m.group('value'),lineNumber,False))
+				if m.group('msgid'):
+					msgid = m.group('msgid')
+				else:
+					msgid = ''
+				array.append(StringLine(m.group('name'), msgid, m.group('value'),lineNumber,False))
 			lineNumber += temp
 		else:
-			array.append(StringLine('UnknownString',read,lineNumber,True))
+			array.append(StringLine('UnknownString','',read,lineNumber,True))
 		read = inFile.readline()
-
 	return array
 
 
+def writeToFile(fileName, array, result, printLine):
+	regex = '"dst":"((?P<index>[^"]+)@(?P<line>[^"]*))"'
+	pattern = re.compile(regex, re.S)
+	# m = pattern.search(str(result))
+	for m in pattern.finditer(result):
+		index = m.group('index')
+		line = m.group('line')
+		line = line.replace('“','"').replace('”','"').replace('\/','/')
+		array[int(index)].setValue(line)
+
+	if printLine:
+		for line in array:
+			print('line: ' + str(line.lineNumber) + '    key: ' + line.key + '    value: ' + line.value)
+
+	preName = fileName.split('.xml')[0] + '_'
+	tamp = time.strftime('%Y%m%d_%H%M%S')
+	outFilename = preName + tamp + ".xml"
+	outFile = open(outFilename, "w", encoding="utf-8")
+	print('结果写入文件: ' + outFilename + '\n')
+	for line in array:
+		if line.key in 'UnknownString':
+			outFile.write(line.value)
+		else:
+			if len(line.msgid) > 0:
+				outFile.write('    <string name="%s" msgid="%s">%s</string>\n' %(line.key, line.msgid, line.value))
+			else:
+				outFile.write('    <string name="%s">%s</string>\n' %(line.key, line.value))
+
+
 def translate(fileName):
-	array = readFileToArray(file)
+	array = readFileToArray(fileName)
 	transArray = []
 	index = 0
 	for line in array:
-		index += 1
 		if line.key not in 'UnknownString':
 			transArray.append(str(index) + '@' + line.value)
+		index += 1
 		continue
 	transStr = ''
 	for i in range(len(transArray)-1):
 		transStr += transArray[i] + '\n'
 	transStr += transArray[-1]
 	print(transStr)
-	trans(transStr)
-	# decode
-	# write back
+	print('需翻译行数: ' + str(len(transArray)) + '\n')
+	result = trans(transStr)
+	print(result)
+
+	writeToFile(fileName, array, result, True)
+
+
+def translateAfterCompare(file1,file2):
+	print('对比文件: ' + file2 + '\n')
+	array = readFileToArray(file1)
+	dict = readFileToDict(file2)
+	for i in (range(len(array))):
+		if(array[i].key in dict.keys()):
+			array[i].value = dict[array[i].key]
+			array[i].isTranslated = True
+			del dict[array[i].key]
+			array[i].key = 'UnknownString'
+
+	transArray = []
+	index = 0
+	for line in array:
+		if line.key not in 'UnknownString' and line.isTranslated == False:
+			transArray.append(str(index) + '@' + line.value)
+		index += 1
+		continue
+	print('需翻译行数: ' + str(len(transArray)) + '\n')
+	transStr = ''
+	for i in range(len(transArray)-1):
+		transStr += transArray[i] + '\n'
+	if len(transArray) > 0:
+		transStr += transArray[-1] 
+	result = trans(transStr)
+	# print(result)
+	print('从对比文件中插入行数: ' + str(len(dict)) + '\n')
+	for key in dict.keys():
+		array.append(StringLine('UnknownString','',dict[key],0,True))
+
+	writeToFile(file1, array, result, False)
 
 
 if __name__ == '__main__':
 	if 'linux2' in sys.platform:
-		file =  "/home/xionghg/test/translate/privacy_strings.xml"
+		file =  "/home/xionghg/test/translate/string.xml"
+		# basefile = '/home/xionghg/SharedFolder/workarea/translate/branch_oversea_pvt_Amigo_SystemManager_AndroidM_PowerDev_V3.0.9_rel/res/values-zh-rTW/'
+		# basefile = '/home/xionghg/SharedFolder/workarea/7558/gionee_packages_apk_amigo_4.0/mtk/packages_6.0/services/Telephony_mt6755c/res/values-zh-rCN/'
 	if 'win32' in sys.platform:
 		file = "D:\strings.xml"
-	print(file)
-	choice = 5
+	print('待翻译文件: ' + file + '\n')
+	choice = 6
 	if choice == 1:
 		readFile(file)
 	if choice == 2:
@@ -201,8 +242,15 @@ if __name__ == '__main__':
 	if choice == 3:
 		array = readFileToArray(file)
 		for line in array:
-			print('line: ' + str(line.lineNumber) + '    key: ' + line.key + '    value: ' + line.value)
+			print('line: ' + str(line.lineNumber) + '  key: ' + line.key + '  msgid: ' + line.msgid + '  value: ' + line.value)
 	if choice == 4:
 		other()
 	if choice == 5:
 		translate(file)
+	if choice == 6:
+		basefile = '/home/xionghg/test/translate/'
+		file = basefile+'strings.xml'
+		file2 = basefile+'strings2.xml'
+		translateAfterCompare(file,file2)
+
+
